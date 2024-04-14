e6ca031449fbfd8222c1a5498c29d9d785b4d2f1
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 11:34:09 +0430

    installing styled-components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 function Navbar() {
   return <div>Navbar page</div>;
 }
+
+const Wrapper = styled.div``;
 
 export default Navbar;
