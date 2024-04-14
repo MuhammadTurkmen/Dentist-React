6ae6a0a8962d38d3b6ca7e8c7302ebdd7b102614
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 11:29:24 +0430

    installing react icons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { About, Home, Prices, Error } from "./pages/index";
+import Navbar from "./components/Navbar";
+import Contact from "./components/contact";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/prices" element={<Prices />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Contact />
     </Router>
   );
 }
