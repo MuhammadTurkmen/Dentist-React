d2d61ae546c5e1869acbf447403f00ec35499f54
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 07:56:02 +0430

    adding h1

--- src/components/DentalServices.js
+++ src/components/DentalServices.js
@@ -2,7 +2,15 @@ import React from "react";
 import styled from "styled-components";
 
 function DentalServices() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <h1></h1>
+      <div className="underline"></div>
+      <div className="container"></div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div``;
 
 export default DentalServices;
