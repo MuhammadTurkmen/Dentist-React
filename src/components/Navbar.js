55471ac312ddf9d1955278e98f6227f5d0a32354
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 13:10:33 +0430

    adding icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import Logo from "../images/logo.svg";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 function Navbar() {
   return (
     <Wrapper>
       <div className="container">
         <img src={Logo} alt="" />
+        <div className="links-container">
+          <Link to="/">Home</Link>
+          <Link to="/about">About US</Link>
+          <Link to="/prices">Prices</Link>
+          <Link to="#Services">Services</Link>
+        </div>
+        <div className="btn-sidbar-container">
+          <Link to="#footer">Kontakt oss</Link>
+          <FaBars />
+        </div>
       </div>
     </Wrapper>
   );
@@ -14,6 +26,9 @@ function Navbar() {
 
 const Wrapper = styled.div`
   background-color: #00415a;
+  .container {
+    display: flex;
+  }
 `;
 
 export default Navbar;
