6566c3a1371511810a5063f9494e86aac89de5a3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 09:08:06 +0430

    adding p and h1

--- src/components/Clear.js
+++ src/components/Clear.js
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 function Clear() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="title-container">
+          <h1 className="title">
+            Clear Dental Clinic - your dentist in <strong>Oslo</strong> and
+            Drammen
+          </h1>
+          <p className="desc">
+            Good dental health is important, whether you are young or old.
+            However, it is also important to find a dentist you feel comfortable
+            with. Clear Dental Clinic has a number of skilled dentists and
+            specialists who will take excellent care of you as a patient.
+          </p>
+        </div>
+        <div className="images-container"></div>
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  background-color: #001f2b;
+  color: #fff;
+  .title {
+    font-weight: 400;
+  }
+`;
 
 export default Clear;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import DentalServices from "../components/DentalServices";
+import Clear from "../components/Clear";
 
 function Home() {
   return (
     <>
       <Hero />
       <DentalServices />
+      <Clear />
     </>
   );
 }
