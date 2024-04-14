3e69e6195ec9354589e1a6c0ddf5d9031551b527
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-14 07:57:08 +0430

    adding font size

--- src/components/DentalServices.js
+++ src/components/DentalServices.js
@@ -4,13 +4,18 @@ import styled from "styled-components";
 function DentalServices() {
   return (
     <Wrapper>
-      <h1></h1>
+      <h1 className="title">Our Dental Services</h1>
       <div className="underline"></div>
       <div className="container"></div>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  .title {
+    text-align: center;
+    font-size: 30px;
+  }
+`;
 
 export default DentalServices;
